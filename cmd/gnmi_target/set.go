858b249a1077d2321d9bd3698ef1d547655d0340
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-01-05 17:09:56 +0100

    Update variable name refering to gnmi

--- cmd/gnmi_target/set.go
+++ cmd/gnmi_target/set.go
@@ -16,20 +16,23 @@ package main
 
 import (
 	"github.com/google/gnxi/utils/credentials"
-	pb "github.com/openconfig/gnmi/proto/gnmi"
+	gnmi "github.com/openconfig/gnmi/proto/gnmi"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 // Set overrides the Set func of gnmi.Target to provide user auth.
-func (s *server) Set(ctx context.Context, req *pb.SetRequest) (*pb.SetResponse, error) {
+func (s *server) Set(ctx context.Context, req *gnmi.SetRequest) (*gnmi.SetResponse, error) {
 	msg, ok := credentials.AuthorizeUser(ctx)
 	if !ok {
 		log.Infof("denied a Set request: %v", msg)
 		return nil, status.Error(codes.PermissionDenied, msg)
 	}
 	log.Infof("allowed a Set request: %v", msg)
+
+	// log.Infof(req.String())
+
 	setResponse, err := s.Server.Set(ctx, req)
 	return setResponse, err
 }
