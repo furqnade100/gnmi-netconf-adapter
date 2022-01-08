1a8fbbf29309da40f94f9b75b9b2078d743a7c49
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-01-08 15:02:44 +0100

    Add data conversion

---
+++ pkg/dataConversion/convert.go
@@ -0,0 +1,48 @@
+package dataConversion
+
+import (
+	"time"
+
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	"github.com/openconfig/gnmi/proto/gnmi"
+
+	sb "github.com/onosproject/gnmi-netconf-adapter/pkg/southbound"
+)
+
+var log = logging.GetLogger("main")
+
+// Takes in a gnmi get request and returns a gnmi get response.
+func Convert(req *gnmi.GetRequest, typeOfRequest string) *gnmi.GetResponse {
+
+	/************************************************************
+	Implementation of data conversion should be implemented here.
+	*************************************************************/
+	// Example of data conversion initiation
+	xmlRequest := json2Xml(req.String())
+
+	var reply = ""
+
+	// Initiate southbound NETCONF client, sending the xml
+	switch typeOfRequest {
+	case "Get":
+		reply = sb.GetConfig(xmlRequest).Data
+	case "Set":
+		reply = sb.UpdateConfig(xmlRequest).Data
+	}
+
+	// Logs the reply, before sending back the response a conversion from xml to json should be implemented.
+	log.Infof(reply)
+
+	// Simulated response.
+	notifications := make([]*gnmi.Notification, 1)
+	prefix := req.GetPrefix()
+	ts := time.Now().UnixNano()
+
+	notifications[0] = &gnmi.Notification{
+		Timestamp: ts,
+		Prefix:    prefix,
+	}
+
+	resp := &gnmi.GetResponse{Notification: notifications}
+	return resp
+}
---
+++ pkg/dataConversion/utils.go
@@ -0,0 +1,68 @@
+package dataConversion
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+var xmlBuilder strings.Builder
+
+func json2Xml(jsonString string) string {
+	xmlBuilder.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
+	xmlBuilder.WriteString("\n<root>")
+
+	// Creating the maps for JSON
+	m := map[string]interface{}{}
+
+	// Parsing/Unmarshalling JSON encoding/json
+	err := json.Unmarshal([]byte(jsonString), &m)
+	if err != nil {
+		panic(err)
+	}
+
+	parseMap(m)
+
+	xmlBuilder.WriteString("\n</root>")
+
+	return xmlBuilder.String()
+}
+
+func parseMap(aMap map[string]interface{}) {
+	for key, val := range aMap {
+		switch concreteVal := val.(type) {
+		case map[string]interface{}:
+			fmt.Println(key)
+			parseMap(val.(map[string]interface{}))
+		case []interface{}:
+			fmt.Println(key)
+			var value = fmt.Sprintf("\n  <%v>", key)
+			xmlBuilder.WriteString(value)
+
+			parseArray(val.([]interface{}))
+
+			value = fmt.Sprintf("</%v>", key)
+			xmlBuilder.WriteString(value)
+		default:
+			var value = fmt.Sprintf("\n  <%v>%v</%v>", key, concreteVal, key)
+			xmlBuilder.WriteString(value)
+		}
+	}
+}
+
+func parseArray(anArray []interface{}) {
+	for _, val := range anArray {
+		switch concreteVal := val.(type) {
+		case map[string]interface{}:
+			xmlBuilder.WriteString("  ")
+			parseMap(val.(map[string]interface{}))
+			xmlBuilder.WriteString("\n  ")
+		case []interface{}:
+			parseArray(val.([]interface{}))
+			xmlBuilder.WriteString("\n")
+		default:
+			var value = fmt.Sprintf("%v", concreteVal)
+			xmlBuilder.WriteString(value)
+		}
+	}
+}
