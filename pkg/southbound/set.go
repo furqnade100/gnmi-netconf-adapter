12307ed370c34bed686e4b8e5bfce015265a4ec8
Jojjer123 <fredrikhallstrom1@hotmail.com>
2022-01-20 16:33:11 +0100

    Update set.go

--- pkg/southbound/set.go
+++ pkg/southbound/set.go
@@ -16,7 +16,7 @@ Example of xml that updates a queue-max-sdu tag.
 *	</interfaces>
  */
 
-const test = "<interfaces xmlns=\"urn:ietf:params:xml:ns:yang:ietf-interfaces\"><interface><name>sw0p5</name><max-sdu-table xmlns=\"urn:ieee:std:802.1Q:yang:ieee802-dot1q-sched\"><traffic-class>0</traffic-class><queue-max-sdu>2000</queue-max-sdu></max-sdu-table></interface></interfaces>"
+const test = "<interfaces xmlns=\"urn:ietf:params:xml:ns:yang:ietf-interfaces\"><interface><name>sw0p5</name><enabled>false</enabled></interface></interfaces>"
 
 // Updates the configuration accoring to the input xml for the target "running"
 func UpdateConfig(xmlChanges string) {
