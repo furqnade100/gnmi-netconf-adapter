8e51abfbda8d86aef714129209a292da553049ef
furqnade100 <fnadeem.cs@gmail.com>
2022-03-04 15:22:55 +0100

    auth2

--- pkg/northbound/admin/fetchPlugins.go
+++ pkg/northbound/admin/fetchPlugins.go
@@ -2,19 +2,33 @@ package admin
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 
-	"github.com/google/gnxi/utils/credentials"
 	"github.com/onosproject/onos-api/go/onos/config/admin"
+	"github.com/onosproject/onos-lib-go/pkg/certs"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func ListPlugins() error {
 	// opts := []grpc.DialOption{
 	// 	grpc.WithInsecure(),
 	// }
+	var opts []grpc.DialOption
+	//opts := credentials.ClientCredentials("onos-config")
+	cert, err := tls.X509KeyPair([]byte(certs.DefaultClientCrt), []byte(certs.DefaultClientKey))
+	if err != nil {
+		fmt.Println("custom: error getting crts")
+		return err
+	}
+	opts = []grpc.DialOption{
+		grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
+			Certificates:       []tls.Certificate{cert},
+			InsecureSkipVerify: true,
+		})),
+	}
 
-	opts := credentials.ClientCredentials("onos-config")
 	conn, err := grpc.Dial("onos-config:5150", opts...)
 	if err != nil {
 		return err
